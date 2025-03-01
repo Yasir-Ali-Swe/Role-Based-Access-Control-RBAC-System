8989f36239d5338ed9d6dcbae9f9f4342c74ef3c
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2025-03-01 12:15:28 +0500

    backend with all api setup

--- backend/Controllers/adminController.js
+++ backend/Controllers/adminController.js
@@ -13,3 +13,18 @@ export const getAllUsers = async (req, res) => {
         return res.status(500).json({ success: false, message: "Internal server error." });
     }
 }
+
+export const deleteUser=async (req,res)=>{
+    try {
+        const userId=req.params.id;
+        const user=await userModel.findById(userId);
+        if(!user){
+            return res.status(404).json({success:false,message:"User not found."});
+        }
+        await userModel.findByIdAndDelete(userId);
+        return res.status(200).json({success:true,message:"User deleted successfully.","deletedUser":user});
+    } catch (error) {
+        console.log(`Error -> ${error}`)
+        return res.status(500).json({ success: false, message: "Internal server error." });
+    }
+}
