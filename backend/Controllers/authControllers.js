f81ee348ec43548b540b93668a6324a7134938f0
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2025-03-01 11:51:45 +0500

    backend with all api setup

--- backend/Controllers/authControllers.js
+++ backend/Controllers/authControllers.js
@@ -4,7 +4,7 @@ import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 
 dotenv.config();
-// const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
+
 
 export const register = async (req, res) => {
     try {
---
+++ backend/Middelwear/verifyIsAdmin.js
@@ -0,0 +1,25 @@
+import userModel from "../Models/userModel.js";
+import dotenv from "dotenv";
+import jwt from "jsonwebtoken";
+
+dotenv.config();
+export const verifyIsAdmin = async (req, res, next) => {
+    try {
+        const token = req.cookies.LoginCookie;
+        if (!token) {
+            return res.status(401).json({ success: false, message: "User is not logged in." });
+        }
+        const decodedToken = jwt.verify(token, process.env.JWT_SECRET_KEY);
+        const user = await userModel.findById(decodedToken.userId);
+        if (!user) {
+            return res.status(401).json({ success: false, message: "User is not logged in." });
+        }
+        if (user.role !== "admin") {
+            return res.status(401).json({ success: false, message: "User is not admin." });
+        }
+        next();
+    } catch (error) {
+        console.log(`Error -> ${error}`)
+        return res.status(500).json({ success: false, message: "Internal server error." });
+    }
+}
