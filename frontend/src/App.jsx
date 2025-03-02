c156d8b07cde3772f5df61764102906494b4e688
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2025-03-02 13:37:39 +0500

    frontend with all api intergration

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-white text-3xl text-center font-bold bg-purple-950 py-4">Yasir Ali</h1>
+
+    </div>
+  );
+};
+
+export default App;
