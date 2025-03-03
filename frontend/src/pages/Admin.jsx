4cfc7f569b2d35d553852c37b9522c1523916c9a
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2025-03-03 08:35:24 +0500

    Complete frontend will all api intigration

--- frontend/src/pages/Admin.jsx
+++ frontend/src/pages/Admin.jsx
@@ -2,9 +2,33 @@ import React from 'react'
 
 const Admin = () => {
   return (
-    <div>
-      <h1>Admin</h1>
+    <div className='bg-purple-950 h-screen flex justify-center'>
+    <div className="ProfileContainer">
+      <h1 className='text-3xl text-white font-bold text-center pt-10'>User Profile</h1>
+      <div className='TableContainer flex justify-center mt-5'>
+        <table>
+          <thead>
+            <tr>
+              <th className='border-2 border-white px-4 py-2 text-white'>Sr</th>
+              <th className='border-2 border-white px-4 py-2 text-white'>Name</th>
+              <th className='border-2 border-white px-4 py-2 text-white'>Email</th>
+              <th className='border-2 border-white px-4 py-2 text-white'>Role</th>
+              <th className='border-2 border-white px-4 py-2 text-white'>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+            <td className='border-2 border-white px-4 py-2 text-white'>1</td>
+              <td className='border-2 border-white px-4 py-2 text-white'>Yasir Ali</td>
+              <td className='border-2 border-white px-4 py-2 text-white'>yasir@gmail.com</td>
+              <td className='border-2 border-white px-4 py-2 text-white'>Admin</td>
+              <td className='border-2 border-white px-4 py-2 text-white'><button className='cursor-pointer bg-red-600 px-3 py-1 border border-white rounded-md text-xl font-bold text-white'>Delete</button></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
+  </div>
   )
 }
 
--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -2,8 +2,28 @@ import React from 'react'
 
 const Profile = () => {
   return (
-    <div>
-      <h1>Profile</h1>
+    <div className='bg-purple-950 h-screen flex justify-center'>
+      <div className="ProfileContainer">
+        <h1 className='text-3xl text-white font-bold text-center pt-10'>User Profile</h1>
+        <div className='TableContainer flex justify-center mt-5'>
+          <table>
+            <thead>
+              <tr>
+                <th className='border-2 border-white px-4 py-2 text-white'>Name</th>
+                <th className='border-2 border-white px-4 py-2 text-white'>Email</th>
+                <th className='border-2 border-white px-4 py-2 text-white'>Role</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td className='border-2 border-white px-4 py-2 text-white'>Yasir Ali</td>
+                <td className='border-2 border-white px-4 py-2 text-white'>yasir@gmail.com</td>
+                <td className='border-2 border-white px-4 py-2 text-white'>Admin</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   )
 }
